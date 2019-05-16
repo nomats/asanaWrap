f6bae97cf1c05a83ffa5856580f9256781c90a16
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-05-16 17:15:22 +0100

    :white_check_mark: add tests for _isPointBetween tests

--- spec/pose_checkers/pose_checker_constructor.test.js
+++ spec/pose_checkers/pose_checker_constructor.test.js
@@ -5,8 +5,35 @@ describe("PoseCheckerConstructor#_isPointBetween", ()=>{
     subject = new PoseCheckerConstructor
   })
 
-  it("correctly evaluates", () => {
-    subject = new PoseCheckerConstructor
+  it("correctly evaluates true examples", () => {
     expect(subject._isPointBetween(5,[3,7])).toEqual(true)
+  })
+
+  it("correctly evaluates true examples with negative numbers", () => {
+    expect(subject._isPointBetween(3,[-2,7])).toEqual(true)
+  })
+
+  it("correctly evaluates true examples", () => {
+    expect(subject._isPointBetween(50,[0,100])).toEqual(true)
+  })
+
+  it("correctly evaluates true examples with negative numbers", () => {
+    expect(subject._isPointBetween(-5,[-1,-10])).toEqual(true)
+  })
+
+  it("correctly evaluates false examples", () => {
+    expect(subject._isPointBetween(1,[3,7])).toEqual(false)
+  })
+
+  it("correctly evaluates true examples with negative numbers", () => {
+    expect(subject._isPointBetween(-3,[-2,7])).toEqual(false)
+  })
+
+  it("correctly evaluates true examples", () => {
+    expect(subject._isPointBetween(-1,[0,100])).toEqual(false)
+  })
+
+  it("correctly evaluates true examples with negative numbers", () => {
+    expect(subject._isPointBetween(0,[-1,-10])).toEqual(false)
   })
 })
