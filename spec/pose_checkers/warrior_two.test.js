ddfb9141d96e5cba70527266c990562f000a313a
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-05-17 11:43:24 +0100

    :sparkles::hankey: add warrior two

--- spec/pose_checkers/warrior_two.test.js
+++ spec/pose_checkers/warrior_two.test.js
@@ -1,29 +1,32 @@
 const sample = require("../posenet_sample_data/warrior_two_test_sample.js");
-const isWarriorTwo = require("../../lib/pose_checker/warriorTwo.js");
+const isWarriorTwo = require("../../lib/pose_checkers/warrior_two.js");
+const PosenetObjectWrapper = require("../../lib/posenet_object_wrapper/main.js")
+
+
 
 describe("#isWarrior2", ()=>{
   it("can recognise correct position", () => {
-    var input = sample["correct"][0];
+    var input = new PosenetObjectWrapper(sample["correct"][0]);
     expect(isWarriorTwo(input)[0]).toEqual(true);
   });
 
   it("can recognise correct position", () => {
-    var input = sample["correct"][1];
+    var input = new PosenetObjectWrapper(sample["correct"][1]);
     expect(isWarriorTwo(input)[0]).toEqual(true);
   });
 
   it("can recognise incorrect position", () => {
-    var input = sample["incorrect"][0];
+    var input = new PosenetObjectWrapper(sample["incorrect"][0]);
     expect(isWarriorTwo(input)[0]).toEqual(false);
   });
 
   it("can recognise incorrect position", () => {
-    var input = sample["incorrect"][1];
+    var input = new PosenetObjectWrapper(sample["incorrect"][1]);
     expect(isWarriorTwo(input)[0]).toEqual(false);
   });
 
   it("can recognise incorrect position", () => {
-    var input = sample["incorrect"][2];
+    var input = new PosenetObjectWrapper(sample["incorrect"][2]);
     expect(isWarriorTwo(input)[0]).toEqual(false);
   });
 })
