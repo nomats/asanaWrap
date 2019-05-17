345bdbad8c03de7373d581cbb911d61c9d06d7fb
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-05-17 11:15:15 +0100

    :recycle: refactor math_model tests

--- spec/math_model.test.js
+++ spec/math_model.test.js
@@ -8,47 +8,39 @@ beforeEach(()=>{
 
 describe("MathModel#angle", () => {
   it('can calculate angles correctly', () => {
-    var subject = new MathModel()
     expect(subject.angle({x: 12, y: 5},{x: 0, y: 0},{x: 12, y: 0})).toEqual(23)
   });
 
   it('can calculate angles correctly', () => {
-    var subject = new MathModel()
     expect(subject.angle({x: 0, y: 2},{x: 5, y: 5},{x: 2, y: 0})).toEqual(28)
   });
 
   it('can calculate angles correctly', () => {
-    var subject = new MathModel()
     expect(subject.angle({x: 2, y: 0},{x: 0, y: 2},{x: 5, y: 5})).toEqual(76)
   });
 })
 
 describe("MathModel#isStraight", ()=>{
   it('can deduce straight lines', () => {
-    var subject = new MathModel()
     expect(subject.isStraight([{x: 2, y: 0},{x: 0, y: 2},{x: 5, y: 5}],1)).toEqual(false)
   });
 
   it('can deduce straight lines', () => {
-    var subject = new MathModel()
     expect(subject.isStraight([{x: 1, y: -0.5},{x: 3,y: -0.6},{x: 5,y: -0.7},{x: 7,y: -0.8},
        {x: 9, y:-0.9}, {x: 11, y:-1.0}],1)).toEqual(true)
   });
 
   it('can deduce straight lines', () => {
-    var subject = new MathModel()
     expect(subject.isStraight([{x: -1, y: -0.5},{x: -1.01,y: -0.6},{x: -1.02,y: -0.7},{x: -1.03,y: -0.8},
        {x: -1.04, y:-0.9}, {x: -1.05, y:-1.0}])).toEqual(true)
   });
 
   it('can deduce straight lines', () => {
-    var subject = new MathModel()
     expect(subject.isStraight([{x: -1, y: 3},{x: 1,y: 3.1},{x: 3,y: 3.6},{x: 5,y: 3.3},
        {x: 7, y:2.9}, {x: 9, y:3.1}],1)).toEqual(true)
   });
 
   it('can deduce straight lines', () => {
-    var subject = new MathModel()
     expect(subject.isStraight([{x: -1, y: -9},{x: -1.01,y: 5},{x: -1.02,y: -3.7},{x: 1.03,y: -0.8},
        {x: 13.04, y:-0.9}, {x: -1.05, y:-1.0}],4)).toEqual(false)
   });
@@ -56,19 +48,16 @@ describe("MathModel#isStraight", ()=>{
 
 describe("MathModel#isHorizontal",()=>{
   it('can deduce horizontal lines', () => {
-    var subject = new MathModel()
     expect(subject.isHorizontal([{x: -1, y: 3},{x: 1,y: 3.1},{x: 3,y: 3.6},{x: 5,y: 3.3},
        {x: 7, y:2.9}, {x: 9, y:2.8}],0.7)).toEqual(true)
   });
 
   it('can deduce non-horizontal lines', () => {
-    var subject = new MathModel()
     expect(subject.isHorizontal([{x: -1, y: 3},{x: 1,y: 3.1},{x: 3,y: 3.9},{x: 5,y: 3.3},
        {x: 7, y:2.9}, {x: 9, y:2.2}],0.7)).toEqual(false)
   });
 
   it('can deduce horizontal lines', () => {
-    var subject = new MathModel()
     expect(subject.isHorizontal([{x: -100, y: 326},{x: 0,y: 328},{x: 100,y: 323},{x: 200,y: 330},
        {x: 300, y:329}],10)).toEqual(true)
   });
@@ -76,19 +65,16 @@ describe("MathModel#isHorizontal",()=>{
 
 describe('MathModel#isVertical',()=>{
   it('can deduce vertical lines', () => {
-    var subject = new MathModel()
     expect(subject.isVertical([{x: 3, y: -1},{x: 3.1,y: 1},{x: 3.6,y: 3},{x: 3.3,y: 5},
        {x: 2.9, y:7}, {x: 2.8, y:9}],0.7)).toEqual(true)
   });
 
   it('can deduce vertical lines', () => {
-    var subject = new MathModel()
     expect(subject.isVertical([{x: 50, y: -1},{x: 48,y: 1},{x: 46,y: 3},{x: 51,y: 5},
        {x: 53, y:7}, {x: 50, y:9}],5)).toEqual(true)
   });
 
   it('can deduce non-vertical lines', () => {
-    var subject = new MathModel()
     expect(subject.isVertical([{x: 50, y: -1},{x: 30,y: 1},{x: 46,y: 3},{x: 51,y: 5},
        {x: 53, y:7}, {x: 72, y:9}],5)).toEqual(false)
   });
