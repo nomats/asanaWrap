a422b275fbaf2f39e2fc835f274355df4ea6a436
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-05-20 10:33:11 +0100

    :sparkles: implement pose class

--- spec/integration_tests/pose.test.js
+++ spec/integration_tests/pose.test.js
@@ -1,10 +1,11 @@
 const Pose = require('../../lib/pose.js')
 const sample = require("../posenet_sample_data/warrior_two_test_sample.js")
+var correct_posenet_sample_object
 var subject
 
 describe("Pose#isWarriorTwo",()=>{
   beforeAll(()=>{
-    var correct_posenet_sample_object = sample.correct[0];
+    correct_posenet_sample_object = sample.correct[0];
   })
   it("can determine a correct warrior two pose", ()=>{
     var subject = new Pose(correct_posenet_sample_object)
