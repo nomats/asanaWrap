fa515b63b558beda24b2c07947b8612add66e00e
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-05-20 10:36:43 +0100

    :white_check_mark: add integration test for Pose#isWarriorTwo

--- spec/integration_tests/pose.test.js
+++ spec/integration_tests/pose.test.js
@@ -1,14 +1,20 @@
 const Pose = require('../../lib/pose.js')
 const sample = require("../posenet_sample_data/warrior_two_test_sample.js")
 var correct_posenet_sample_object
+var incorrect_posenet_sample_object
 var subject
 
 describe("Pose#isWarriorTwo",()=>{
   beforeAll(()=>{
     correct_posenet_sample_object = sample.correct[0];
+    incorrect_posenet_sample_object = sample.incorrect[0];
   })
   it("can determine a correct warrior two pose", ()=>{
     var subject = new Pose(correct_posenet_sample_object)
     expect(subject.isWarriorTwo()[0]).toEqual(true)
+  })
+  it("can determine an incorrect warrior two pose", ()=>{
+    var subject = new Pose(incorrect_posenet_sample_object)
+    expect(subject.isWarriorTwo()[0]).toEqual(false)
   })
 })
